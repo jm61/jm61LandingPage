383cde9e6cf21e7f48574924dd4fcbe6505a406d
jm61 <jean.moirano@gmail.com>
2022-05-15 15:54:57 +0800

    navbar and footer components added

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,19 @@
+<script>
+  import TwitterIcon from "./twitter-icon.svelte";
+</script>
+<footer class="p-4 footer bg-primary text-primary-content footer-center">
+    <div class="grid grid-flow-col gap-4">
+      <a class="link link-hover" href="/about">About</a> 
+      <a class="link link-hover" href="/contact">Contact</a> 
+    </div> 
+    <div>
+      <div class="grid grid-flow-col gap-4">
+        <a href="twitter">
+          <TwitterIcon />
+        </a> 
+      </div>
+    </div> 
+    <div>
+      <p>Copyright © {`${new Date().getFullYear()}`} - All right reserved by JM61.DEV</p>
+    </div>
+  </footer>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,11 @@
 <script>
 	import '../app.css';
+	import Footer from '$lib/components/footer.svelte';
+	import Navbar from '$lib/components/navbar.svelte';
 </script>
 
+<Navbar />
 <main class="container max-w-3xl mx-auto px-4 mb-20">
 	<slot />
 </main>
+<Footer />
