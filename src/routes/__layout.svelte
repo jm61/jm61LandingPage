6a10f6dd4a3644428158777bf2f208a8189328ba
jm61 <jean.moirano@gmail.com>
2022-05-15 16:14:36 +0800

    Theme select added

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,12 @@
 	import '../app.css';
 	import Footer from '$lib/components/footer.svelte';
 	import Navbar from '$lib/components/navbar.svelte';
+	import { themeChange } from 'theme-change'
+	import {onMount} from 'svelte'
+
+	onMount(async () => {
+		themeChange(false)
+	})
 </script>
 
 <Navbar />
